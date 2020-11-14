1281fa0e56843d66be3833ba8e0b102dd1679acd
kasosgeo <kasos.geo@gmail.com>
2020-11-14 09:55:07 +0200

    Update stopArk.sh

--- stopArk.sh
+++ stopArk.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+# ------------------------------------------------------------------
+# Ark Survival Evolved world backup and update by Aerico DonDonDouk
+# ------------------------------------------------------------------
+#
+# We're going to do a backup, then update the Ark Survival Evolved Linux Dedicated server.
+#
+#
+#
+#
+# Now some system binaries...
 STATUS="$(systemctl is-active ark.service)"
 
 if [ "${STATUS}" = "active" ]; then
